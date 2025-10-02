dd959745294916723096c0560baa5d62a68fd87c
in9352 <alix.bergeret@wlv.ac.uk>
2025-10-02 13:50:18 +0100

    Added table

--- week02.php
+++ week02.php
@@ -3,5 +3,17 @@
 <?php include("templates/nav.php");?>
 
 <h1>Hello, world!</h1>
+<p>Hi</p>
+
+<?php
+	// Define a simple array
+	$cars = array("Volvo", "BMW", "Toyota");
+?>
+
+<table class="table table-striped">
+	<?php foreach($cars as $one_car):?>
+		<tr><td><?=$one_car?></td><tr>
+	<?php endforeach;?>
+</table>
 
 <?php include("templates/footer.php");?>		
